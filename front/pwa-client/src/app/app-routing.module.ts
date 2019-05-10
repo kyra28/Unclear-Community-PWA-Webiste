dbe57114a2da508450a13afe474d27abaffdc5db
JihaneSbaytti <sbaytti.jihane@gmail.com>
2019-05-10 21:32:53 +0200

    Created routes for tab and implemented a routing system for the navbar

--- front/pwa-client/src/app/app-routing.module.ts
+++ front/pwa-client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./components/tabs/home/home.component";
+import {TeamsComponent} from "./components/tabs/teams/teams.component";
+import {MatchesComponent} from "./components/tabs/matches/matches.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'teams', component: TeamsComponent},
+  { path: 'teams', component: MatchesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
