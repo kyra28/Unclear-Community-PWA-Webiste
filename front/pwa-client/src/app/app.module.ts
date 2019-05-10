07b3a2a8f73e5623f28529a20d3ca584ca8eb415
JihaneSbaytti <sbaytti.jihane@gmail.com>
2019-05-10 19:42:11 +0200

    Implementing layout components (header, footer, sidebar, slider..)

--- front/pwa-client/src/app/app.module.ts
+++ front/pwa-client/src/app/app.module.ts
@@ -5,10 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SliderComponent } from './components/tabs/home/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import {HomeComponent} from "./components/tabs/home/home.component";
+import { NewsComponent } from './components/tabs/home/news/news.component';
+import { TeamsComponent } from './components/tabs/teams/teams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SliderComponent,
+    HomeComponent,
+    SidebarComponent,
+    TabsComponent,
+    NewsComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
