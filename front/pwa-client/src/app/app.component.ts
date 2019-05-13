c5015f7e91634a084f681b16d69b14ec030fedd1
JihaneSbaytti <sbaytti.jihane@gmail.com>
2019-05-13 19:38:00 +0200

    Making the pwa full screen on iOs and Android, adding missing assets

--- front/pwa-client/src/app/app.component.ts
+++ front/pwa-client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+declare let $: any;
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,8 @@ import {Router} from "@angular/router";
 })
 export class AppComponent {
   title = 'pwa-client';
+  ngOnInit() {
+    $('body').addClass('df');
+  }
   constructor(public router: Router){}
 }
